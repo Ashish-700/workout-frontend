a46518077bb15426734427f63013380e258c22d5
Ashish <ashdhs50@gmail.com>
2023-05-01 14:16:07 +0530

    changed the proxy value in Json, beacuse the api was moved to render.com

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts/", {
+      const response = await fetch("/api/workouts", {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
